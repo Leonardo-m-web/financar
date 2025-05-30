9aaa7b24b7fe06da2a85c74ed9f0de2e51d0e1d8
Leonardo-m-web <leonardo.m.j.soares@gmail.com>
2025-05-30 22:10:00 +0000

    att1

---
+++ public/car.js
@@ -0,0 +1,64 @@
+const carro = [
+  {
+    "id": 1,
+    "marca": "Toyota",
+    "modelo": "Corolla",
+    "ano": 2022,
+    "placa": "ABC1D23",
+    "cor": "Prata",
+    "km": 15000,
+    "tipoComb": "Flex",
+    "cambio": "Automático",
+    "valor": 95000.00,
+    "dataCadastro": "2023-10-15",
+    "motor": "1.0",
+    "opcionais": [
+      "Ar-condicionado",
+      "Vidros elétricos",
+      "Trava elétrica",
+      "Direção hidráulica",
+      "Sensor de estacionamento",
+      "GPS",
+      "teto solar"
+    ],
+    "imagens":{
+      "img1": "img/carro.png",
+      "img2": "img/carro.png",
+      "img3": "img/carro.png",
+      "img4": "img/carro.png",
+      "img5": "img/carro.png",
+      "img6": "img/carro.png"
+    }
+  },
+  {
+    "id": 2,
+    "marca": "Honda",
+    "modelo": "Civic",
+    "ano": 2021,
+    "placa": "XYZ9W87",
+    "cor": "Preto",
+    "km": 20000,
+    "tipoComb": "Flex",
+    "cambio": "Automático",
+    "valor": 85000.00,
+    "dataCadastro": "2023-09-20",
+    "motor": "1.5",
+    "opcionais": [
+      "Ar-condicionado",
+      "Vidros elétricos",
+      "Trava elétrica",
+      "Direção elétrica",
+      "Câmera de ré"
+    ],
+    "imagens":{
+      "img1": "img/carro2.jpeg",
+      "img2": "img/carro2.jpeg",
+      "img3": "img/carro2.jpeg",
+      "img4": "img/carro2.jpeg",
+      "img5": "img/carro2.jpeg",
+      "img6": "img/carro2.jpeg"
+    }
+  }
+];
+
+
